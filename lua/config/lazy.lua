4feadc0bb31e9d75ac87a13ed57554a981cb505a
snowykr <snowykr22@gmail.com>
2025-10-24 01:27:55 +0900

    update windows wsl config

--- lua/config/lazy.lua
+++ lua/config/lazy.lua
@@ -19,6 +19,7 @@ vim.opt.clipboard = 'unnamedplus'
 -- Make sure to setup `mapleader` and `maplocalleader` before
 -- loading lazy.nvim so that mappings are correct.
 -- This is also a good place to setup other settings (vim.opt)
+vim.api.nvim_set_keymap('n', '<Space>', '<Nop>', { noremap = true, silent = true })
 vim.g.mapleader = " "
 vim.g.maplocalleader = " "
 
